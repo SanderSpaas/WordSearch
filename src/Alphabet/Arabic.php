03f59a71e72de46c484f05848fee5d2e53e93575
Sander Spaas <80250065+SanderSpaas@users.noreply.github.com>
2024-01-22 12:50:14 +0100

    Create Arabic.php

---
+++ src/Alphabet/Arabic.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace WordSearch\Alphabet;
+
+use WordSearch\Alphabet;
+
+class Arabic extends Alphabet
+{
+    protected $alphabet = [
+        'ا',
+        'ب',
+        'ت',
+        'ث',
+        'ج',
+        'ح',
+        'خ',
+        'د',
+        'ذ',
+        'ر',
+        'ز',
+        'س',
+        'ش',
+        'ص',
+        'ض',
+        'ط',
+        'ظ',
+        'ع',
+        'غ',
+        'ف',
+        'ق',
+        'ك',
+        'ل',
+        'م',
+        'ن',
+        'ه',
+        'و',
+        'ي'
+    ];
+}
