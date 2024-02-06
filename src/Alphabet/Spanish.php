75ac8424d3131370bcd8b206dea9694843d83124
Sander Spaas <80250065+SanderSpaas@users.noreply.github.com>
2024-02-06 10:41:34 +0100

    added support for Spanish and Ukranian

---
+++ src/Alphabet/Spanish.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace WordSearch\Alphabet;
+
+use WordSearch\Alphabet;
+
+class Spanish extends Alphabet
+{
+    protected $alphabet = [
+        'a',
+        'b',
+        'c',
+        'd',
+        'e',
+        'f',
+        'g',
+        'h',
+        'i',
+        'j',
+        'k',
+        'l',
+        'm',
+        'n',
+        'ñ',
+        'o',
+        'p',
+        'q',
+        'r',
+        's',
+        't',
+        'u',
+        'v',
+        'w',
+        'x',
+        'y',
+        'z',
+    ];
+}
---
+++ src/Alphabet/Ukranian.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace WordSearch\Alphabet;
+
+use WordSearch\Alphabet;
+
+class Ukrainian extends Alphabet
+{
+    protected $alphabet = [
+        'а',
+        'б',
+        'в',
+        'г',
+        'ґ',
+        'д',
+        'е',
+        'є',
+        'ж',
+        'з',
+        'и',
+        'і',
+        'ї',
+        'й',
+        'к',
+        'л',
+        'м',
+        'н',
+        'о',
+        'п',
+        'р',
+        'с',
+        'т',
+        'у',
+        'ф',
+        'х',
+        'ц',
+        'ч',
+        'ш',
+        'щ',
+        'ь',
+        'ю',
+        'я',
+    ];
+}
